ef103da37cd47b8d77f6269e5a4982106d669c00
jinzhuge <sleepingpig9582@hotmail.com>
2020-01-05 08:59:21 +0800

    first jdbcTemplate

---
+++ src/test/java/com/example/jdbcTemplatedemo/JdbcTemplateDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbcTemplatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTemplateDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
